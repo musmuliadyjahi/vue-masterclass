fd96f357653acee202df8e96820f83f2ab4e8800
Musmuliady Jahi <musmuliadyjahi@gmail.com>
2020-01-25 09:24:09 +0700

    impor data source

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
-    <h1>Halo halo</h1>
+    <h1>Hello Vue</h1>
+    <div v-for="thread in threads" :key="thread">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{ users[posts[postsId]] }}</p>
+        <p>{{ posts[postId].text }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import sourceData from '../';
+import sourceData from '@/data';
+console.log(sourceData);
+
 export default {
   name: 'HelloWorld',
   data() {
-    return {};
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    };
   }
 };
 </script>
